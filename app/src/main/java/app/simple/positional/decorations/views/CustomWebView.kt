b7cbea2a55c23382598af2e5caf328e67e8381e3
Hamza Rizwan <hamzarizwan243@gmail.com>
2023-01-13 14:26:05 +0530

    Build166
      - Fixed dark web pages issue

--- app/src/main/java/app/simple/positional/decorations/views/CustomWebView.kt
+++ app/src/main/java/app/simple/positional/decorations/views/CustomWebView.kt
@@ -4,9 +4,9 @@ package app.simple.positional.decorations.views
 
 import android.content.Context
 import android.content.res.Configuration
+import android.os.Build
 import android.util.AttributeSet
 import android.webkit.WebView
-import android.widget.Toast
 import androidx.webkit.WebSettingsCompat
 import androidx.webkit.WebViewFeature
 
@@ -17,13 +17,25 @@ open class CustomWebView(context: Context, attributeSet: AttributeSet) : WebView
         settings.allowFileAccess = true
         settings.setSupportZoom(true)
 
-        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
-            if (context.resources.configuration.uiMode and
-                    Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES) {
-                WebSettingsCompat.setForceDark(this.settings, WebSettingsCompat.FORCE_DARK_ON)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (WebViewFeature.isFeatureSupported(WebViewFeature.ALGORITHMIC_DARKENING)) {
+                WebSettingsCompat.setAlgorithmicDarkeningAllowed(this.settings, context.resources.configuration.uiMode and
+                        Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES)
+            } else if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+                if (context.resources.configuration.uiMode and
+                        Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES) {
+                    @Suppress("DEPRECATION")
+                    WebSettingsCompat.setForceDark(this.settings, WebSettingsCompat.FORCE_DARK_ON)
+                }
             }
         } else {
-            Toast.makeText(context, "If you are having trouble viewing this make sure you are using the latest WebView", Toast.LENGTH_LONG).show()
+            if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+                if (context.resources.configuration.uiMode and
+                        Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES) {
+                    @Suppress("DEPRECATION")
+                    WebSettingsCompat.setForceDark(this.settings, WebSettingsCompat.FORCE_DARK_ON)
+                }
+            }
         }
     }
 }
