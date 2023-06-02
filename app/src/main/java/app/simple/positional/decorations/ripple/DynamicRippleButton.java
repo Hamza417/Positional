386f6b23aba56e20e7f47df985bac0b6b29b8018
Hamza417 <HamzaRizwan243@gmail.com>
2023-06-02 22:37:15 +0530

    Build177
      - Updated button style

--- app/src/main/java/app/simple/positional/decorations/ripple/DynamicRippleButton.java
+++ app/src/main/java/app/simple/positional/decorations/ripple/DynamicRippleButton.java
@@ -4,10 +4,19 @@ import android.content.Context;
 import android.graphics.Color;
 import android.util.AttributeSet;
 
+import androidx.core.content.res.ResourcesCompat;
+
+import app.simple.positional.R;
+import app.simple.positional.util.ColorUtils;
+
 public class DynamicRippleButton extends androidx.appcompat.widget.AppCompatButton {
     public DynamicRippleButton(Context context, AttributeSet attrs) {
         super(context, attrs);
         setBackgroundColor(Color.TRANSPARENT);
+        setAllCaps(false);
+        setTypeface(ResourcesCompat.getFont(context, R.font.bold));
+        setTextColor(ColorUtils.INSTANCE.resolveAttrColor(getContext(), R.attr.colorAppAccent));
+
         if (!isInEditMode()) {
             setBackground(Utils.getRippleDrawable(getContext(), getBackground(), 2F));
         }
